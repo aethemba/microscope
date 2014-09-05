964689e4b1c48f049ab6b5d25d03d133594f0120
Aksel Ethem <aksel.ethem@gmail.com>
2014-09-05 14:03:27 +0200

    almost finished chapter 7

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,16 +1,5 @@
-var postsData = [
-	{
-		title: "Introducing Telescope",
-		author: "Sascha Greiff",
-		url: "http://saschagreiff.com/introducing-telescope"
-	},
-	{
-		title: "Meteor",
-		author: "Tom Coleman",
-		url:  "http://meteor.com"
-	},
-]
-
 Template.postsList.helpers({
-	posts: postsData
+  posts: function() {
+    return Posts.find({}, {sort: {submitted: -1}});
+  }
 });
